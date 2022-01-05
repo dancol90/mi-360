4dbe7398961fb2c584c413cd74b5aa6ab142e6cb
Daniele Colanardi <dancol90@gmail.com>
2022-01-05 17:07:51 +0100

    Bump to 0.6.0

--- Source/mi-360/Properties/AssemblyInfo.cs
+++ Source/mi-360/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.2")]
+[assembly: AssemblyVersion("0.6.0")]
