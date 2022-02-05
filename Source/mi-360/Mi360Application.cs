2fb16a213390970c0bdfd1b2a64ba266320f02f3
Daniele Colanardi <dancol90@gmail.com>
2022-02-05 23:40:55 +0100

    Enable hiding on HidHide that is disabled by default

--- Source/mi-360/Mi360Application.cs
+++ Source/mi-360/Mi360Application.cs
@@ -50,11 +50,12 @@ namespace mi360
             {
                 var success = hh.WhitelistCurrentApplication();
 
-                if (!success)
+                if (success)
+                    hh.EnableHiding = true;
+                else
                     MessageBox.Show("HidHide is not installed or it's currently used by another application. Device hiding will not work properly.", "HidHid unavailable", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
 
-            InitializeComponents();
+            }
 
             _Logger.Information("mi-360 is running");
             CanRun = true;
